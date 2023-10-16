6ca5ffd5b20e313d5ac744ef34c8ad6d85eb3c37
Kyle S <kyles@MacBook-Pro.local>
2023-10-16 11:20:08 -0400

    persist UTMs across page clicks

--- src/global.js
+++ src/global.js
@@ -22,3 +22,27 @@ async function sha256(text) {
     }
       return null;
 }
+
+
+// Get UTMs for this session
+const urlParams = new URLSearchParams(window.location.search);
+const utmSource = urlParams.get('utm_source');
+const utmMedium = urlParams.get('utm_medium');
+const utmCampaign = urlParams.get('utm_campaign');
+const utmContent = urlParams.get('utm_content');
+
+// Store them in session storage
+sessionStorage.setItem('utm_source', utmSource);
+sessionStorage.setItem('utm_medium', utmMedium);
+sessionStorage.setItem('utm_campaign', utmCampaign);
+sessionStorage.setItem('utm_content', utmContent);
+
+// If the current URL does not have UTMs, but we have UTMs in session storage, add them to the URL
+if (!utmSource && sessionStorage.getItem('utm_source')) {
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set('utm_source', sessionStorage.getItem('utm_source'));
+    currentUrl.searchParams.set('utm_medium', sessionStorage.getItem('utm_medium'));
+    currentUrl.searchParams.set('utm_campaign', sessionStorage.getItem('utm_campaign'));
+    currentUrl.searchParams.set('utm_content', sessionStorage.getItem('utm_content'));
+    window.history.replaceState({}, '', currentUrl);
+}
