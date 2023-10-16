01ef03104c2dc9e46056efee19db0b2f1a490daf
Kyle S <kyles@MacBook-Pro.local>
2023-10-16 12:57:07 -0400

    more console logs

--- src/home.js
+++ src/home.js
@@ -1,26 +1,34 @@
 async function handleHomeCTASubmit(submitButtonText) {
+    console.log('Submit button clicked')
+
     const email = document.getElementById("home-cta-email").value;
-    const anonymousId = getCookieValue('ajs_anonymous_id');
     const hubspotUtk = getCookieValue('hubspotutk');
     const fbp = getCookieValue('_fbp');
     const fbc = getCookieValue('_fbc');
     
     const email_hash = await sha256(email);
 
+    const formData = { 
+        form_type: 'home_cta',
+        email: email,
+        email_hash: email_hash,
+        hubspotutk: hubspotUtk,
+        fbp: fbp,
+        fbc: fbc,
+        button_text: submitButtonText
+    }
+
+    console.log('formData: ', formData);
+
     if (email) {
+        console.log('Submit button clicked')
         await segmentIdentify({
             email: email
-        });
+        })
+            .then(() => console.log('segmentIdentify called'));
         
-        await segmentTrack({ 
-            form_type: 'home_cta',
-            email: email,
-            email_hash: email_hash,
-            hubspotutk: hubspotUtk,
-            fbp: fbp,
-            fbc: fbc,
-            button_text: submitButtonText
-        });
+        await segmentTrack(formData)
+            .then(() => console.log('segmentTrack called'));
 
         // Google Ads conversion
         window.gtag('event', 'conversion', {'send_to': 'AW-618863666/W55HCJDW7esYELK4jKcC'});
@@ -34,6 +42,7 @@ async function handleHomeCTASubmit(submitButtonText) {
 document.addEventListener('DOMContentLoaded', function() {
     const buttonElement = document.getElementById("home-cta-button");
     if (buttonElement) {
+        console.log('Found submit button')
         buttonElement.addEventListener('click', handleHomeCTASubmit(buttonElement.value));
     }
 });
